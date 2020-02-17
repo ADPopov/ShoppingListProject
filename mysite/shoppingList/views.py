c09f52ce38e8ad8c09854f9ff8f291aed1be1814
Alexander Popov <AlexPopov1999@gmail.com>
2020-02-17 18:30:05 +0500

    Create migration and admin page

--- mysite/shoppingList/views.py
+++ mysite/shoppingList/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
-
+from django.http import JsonResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello!")
 # Create your views here.
