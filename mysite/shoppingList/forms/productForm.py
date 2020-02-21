ec60dd9d228cbf21dd420762813068ced25bce95
Alexander Popov <AlexPopov1999@gmail.com>
2020-02-21 22:13:22 +0500

    Changed type of dateTime input to DateTime using widget

--- mysite/shoppingList/forms/productForm.py
+++ mysite/shoppingList/forms/productForm.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, DateTimeInput
 
 from shoppingList.models import Product
 
@@ -8,7 +8,7 @@ class ProductDeskForm(forms.Form):
     quantity = forms.IntegerField(label='Количество')
     name = forms.CharField(label='Название', max_length=200),
     shopName = forms.CharField(label='Название магазина', max_length=30),
-    purchase_date = forms.DateTimeField(label='Дата покупки'),
+    purchase_date = forms.DateTimeInput(),
     cost = forms.IntegerField(label='Стоимость'),
 
 
@@ -16,3 +16,6 @@ class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'quantity', 'shopName', 'purchase_date', 'cost']
+        widgets = {
+            'purchase_date': DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
